9d8946c6a9c5e64d9f9d8a929b2e98f60d830f5b
Simon Pirko <simonpirko@ridelex.io>
2021-09-23 22:45:54 +0300

    Initial commit

---
+++ src/main/java/com/example/bookstorec06/BookStoreC06Application.java
@@ -0,0 +1,13 @@
+package com.example.bookstorec06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreC06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreC06Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookstorec06/BookStoreC06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookstorec06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreC06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
