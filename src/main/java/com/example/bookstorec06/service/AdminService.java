9344471fac51d94c99ef8081d4c1a0059edb2694
VitaliyDudnik <82210074+VitaliyDudnik@users.noreply.github.com>
2021-09-30 11:54:26 +0300

    Update AdminService.java

--- src/main/java/com/example/bookstorec06/service/AdminService.java
+++ src/main/java/com/example/bookstorec06/service/AdminService.java
@@ -14,38 +14,64 @@ import java.util.Optional;
 public class AdminService {
     private final AdminRepository adminRepository;
     private final OrderRepository orderRepository;
+    private final BookRepository bookRepository;
 
     public Optional<User> findById(long id) {
         return adminRepository.findById(id);
     }
 
-    public void saveOrUpdate(User user) {
-       adminRepository.save(user);
+    public void saveOrUpdateUser(User user) {
+        adminRepository.save(user);
     }
 
-    public List<User> findAllUser(){
-      return adminRepository.findAll();
+    public List<User> findAllUser() {
+        return adminRepository.findAll();
     }
 
     public List<Order> findAllOrder() {
         return orderRepository.findAll();
     }
-    public void deleteUserById(long id){
-        Optional<User> user = adminRepository.findById(id);
+
+    public Optional<Order> findOrderById(long id) {
+        return orderRepository.findById(id);
+    }
+
+    public void deleteUserById(long id) {
+        Optional<User> user = adminRepositoryfindById(id);
         if (user.isPresent()) {
             adminRepository.deleteById(id);
-        }else {
+        } else {
             throw new MyNotFoundException("Wrong user id for deleteUserById " + id);
         }
     }
 
-    public void deleteOrderById(long id){
+    public void deleteOrderById(long id) {
         Optional<Order> order = orderRepository.findById(id);
         if (order.isPresent()) {
             orderRepository.deleteById(id);
-        }else {
+        } else {
             throw new MyNotFoundException("Wrong order id for deleteOrderById " + id);
         }
     }
 
+    public void deleteBookById(long id) {
+        Optional<Book> book = bookRepository.findById(id);
+        if (book.isPresent()) {
+            bookRepository.deleteById(id);
+        } else {
+            throw new MyNotFoundException("Wrong book id for deleteBookById " + id);
+        }
+    }
+
+    public void saveBook(Book book) {
+        bookRepository.save(book);
+    }
+
+    public List<Book> findAllBook() {
+        return bookRepository.findAll();
+    }
+
+    public Optional<Book> findBookById(long id) {
+        return bookRepository.findById(id);
+    }
 }
