3699cae8ac27ca1e2dc37979deb34126e7185a4d
VitaliyDudnik <82210074+VitaliyDudnik@users.noreply.github.com>
2021-09-30 11:56:14 +0300

    Delete src/main/java/com/example/bookstorec06/repository directory

--- src/main/java/com/example/bookstorec06/repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bookstorec06.repository;
-
-import com.example.bookstorec06.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<User, Long> {
-
-}
